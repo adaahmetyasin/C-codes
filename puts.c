af96f6e23eb283be2f1d7be4757c257a622c62dc
adaahmetyasin <adaahmetyasinedu@yahoo.com>
2021-09-14 23:07:43 +0300

    puts statement

---
+++ puts.c
@@ -0,0 +1,17 @@
+//'\n' means jump to the next line
+// if we use 'printf' and we want to jump to the next line, we should use '\n'
+// but when you use 'puts', you don't have to use '\n'
+#include<stdio.h>
+
+int main(){
+
+    printf("Hello world \n");
+
+    puts("Hello world");
+
+    printf("Hello world");
+
+
+
+return 0;
+}
