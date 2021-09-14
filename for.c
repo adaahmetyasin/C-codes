f563b683a8b70e7d25c09184293341d5fff64492
adaahmetyasin <adaahmetyasinedu@yahoo.com>
2021-09-14 23:15:33 +0300

    for

---
+++ for.c
@@ -0,0 +1,24 @@
+//compound interest calculation
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+    double amount;
+    double principal = 1000.00;
+    double rate = .05;
+    unsigned int year;
+
+    printf("%4s%21s\n", "Year" , "Amount on deposit");
+
+    for( year = 1; year <= 10; year++){
+
+        amount = principal * pow(1.0 + rate, year);
+        printf( "%4u%21.2f\n",year, amount);
+    }
+
+    
+
+
+    return 0;
+}
