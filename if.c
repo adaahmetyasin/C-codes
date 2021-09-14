a1e49acac315eab777aaf46bcc3889a7a0184173
adaahmetyasin <adaahmetyasinedu@yahoo.com>
2021-09-14 23:13:21 +0300

    run if

---
+++ if.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    int grade;
+
+    printf("Enter yout grade: ");
+    scanf("%d",&grade);
+
+    if ( grade >= 60){
+
+        printf("passed");
+    }
+
+
+
+    return 0;
+}
