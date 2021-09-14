bdc11e781f8d2d211718e2c46e19c0cbeee62480
adaahmetyasin <adaahmetyasinedu@yahoo.com>
2021-09-14 23:23:28 +0300

    rhombus

---
+++ rhombus.c
@@ -0,0 +1,49 @@
+
+#include<stdio.h>
+int main()
+{
+
+	int i, j;
+	int enter, star, space;
+
+	printf("Enter:");
+	scanf("%d", &enter);
+	
+	star = 1;
+	space = enter - 1;
+
+	for (i = 0; i < enter; i++)
+	{
+		for ( j = 0; j < space; j++)
+		{
+			printf(" ");
+		}
+		for ( j = 0; j < star; j++)
+		{
+			printf("*");
+		}
+	printf("\n");
+	star += 2;
+	space--;	
+	}	
+
+	star = star - 4;
+	space = space + 2;
+
+
+	for (i = 0; i < enter; i++)
+	{
+		for ( j = 0; j < space; j++)
+		{
+			printf(" ");
+		}
+		for ( j = 0; j < star; j++)
+		{
+			printf("*");
+		}
+	printf("\n");
+	star -= 2;
+	space ++;	
+	}	
+return 0;
+}
