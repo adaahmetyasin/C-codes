224ed7df1326b6ccb4eddff12d657afec7fdd390
adaahmetyasin <adaahmetyasinedu@gmail.com>
2021-09-17 12:47:58 +0300

    while function

---
+++ while.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+int main(){
+     
+    int product = 3;
+
+    while (product <= 100)
+    {
+        product = 3 * product;
+    }
+    printf("%d",product);
+
+
+    return 0;
+}
