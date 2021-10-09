ff98e75a49b8da94c3414e8072b50a40c16064ab
adaahmetyasin <adaahmetyasinedu@gmail.com>
2021-10-09 21:16:15 +0300

    find prime numbers

---
+++ primeNumbers.c
@@ -0,0 +1,32 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int number, i, j, situation;
+    
+
+    printf("Please enter a number: ");
+    scanf("%d",&number);
+    for (i = 2; i <= number; i++)
+    {
+        situation = 1;
+        for (j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                situation = 0;
+                printf("%d isn't a premium number.\n",i);
+                break;
+            }
+            
+        }
+        if (situation == 1)
+        {
+            printf("%d is a premium number.\n",i);
+        }
+        
+    }
+    
+    return 0;
+}
